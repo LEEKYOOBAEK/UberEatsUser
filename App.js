a2c6dc51d72ffe882ccf1c4ca881f2537c3c816e
LeeKyooBaek <charisma1124@gmail.com>
2022-08-10 02:18:08 +0900

    expo 생성 및 image 가져오기, title, subtitle 추가

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    {/* Restaurant item */}
+      <View style={styles.restaurantContainer}>
+        <Image 
+          source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg'}} 
+          style={styles.image}
+        />
+      <Text style={styles.title}>"El Cabo Coffe Bar Tres De May</Text>
+      <Text style={styles.subtitle}>$ $1.99 15-30 minutes</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +24,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10,
+  },
+  restaurantContainer: {
+    width: "100%",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 5/3,
+    marginBottom: 5,
+  },
+  title: {
+    fontSize: 16, 
+    fontWeight: "500",
+    marginVertical: 5,
+  },
+  subtitle: {
+    color: "grey",
   },
 });
