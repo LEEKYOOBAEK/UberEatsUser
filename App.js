e525ec02f964618d05bc1d8d41331f734f8558e7
LeeKyooBaek <charisma1124@gmail.com>
2022-08-10 02:41:53 +0900

    컴포넌트 분리

--- App.js
+++ App.js
@@ -1,18 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import RestaurantItem from './src/components/RestaurantItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
     {/* Restaurant item */}
-      <View style={styles.restaurantContainer}>
-        <Image 
-          source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg'}} 
-          style={styles.image}
-        />
-      <Text style={styles.title}>"El Cabo Coffe Bar Tres De May</Text>
-      <Text style={styles.subtitle}>$ $1.99 15-30 minutes</Text>
-      </View>
+      <RestaurantItem />
+      <RestaurantItem />
+      <RestaurantItem />
       <StatusBar style="auto" />
     </View>
   );
@@ -25,21 +21,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 10,
-  },
-  restaurantContainer: {
-    width: "100%",
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 5/3,
-    marginBottom: 5,
-  },
-  title: {
-    fontSize: 16, 
-    fontWeight: "500",
-    marginVertical: 5,
-  },
-  subtitle: {
-    color: "grey",
   },
 });
---
+++ src/components/RestaurantItem/index.js
@@ -0,0 +1,37 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const RestaurantItem = () => {
+  return (
+    <View style={styles.restaurantContainer}>
+        <Image 
+          source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg'}} 
+          style={styles.image}
+        />
+      <Text style={styles.title}>"El Cabo Coffe Bar Tres De May</Text>
+      <Text style={styles.subtitle}>$ $1.99 15-30 minutes</Text>
+      </View>
+  )
+};
+
+export default RestaurantItem;
+
+const styles = StyleSheet.create({
+    restaurantContainer: {
+      width: "100%",
+      marginVertical: 10,
+    },
+    image: {
+      width: "100%",
+      aspectRatio: 5/3,
+      marginBottom: 5,
+    },
+    title: {
+      fontSize: 16, 
+      fontWeight: "500",
+      marginVertical: 5,
+    },
+    subtitle: {
+      color: "grey",
+    },
+  });
+  
