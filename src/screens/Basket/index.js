8db6c1663cca63713399043a625964e68b5f02bb
LeeKyooBaek <charisma1124@gmail.com>
2022-08-14 00:23:20 +0900

    장바구니에 아이템 뿌리기

--- src/screens/Basket/index.js
+++ src/screens/Basket/index.js
@@ -1,36 +1,41 @@
 import { useState } from 'react';
-import { View, Image, Text, StyleSheet } from 'react-native';
+import { View, FlatList, Text, StyleSheet } from 'react-native';
 import restaurants from "../../../assets/data/restaurants.json";
 import {AntDesign} from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
 
+
 const restaurant = restaurants[0]
 
+const BasketDishItem = ({basketDish}) => {
+    return (
+        <View style={styles.row}>
+
+                <View style={styles.quantityContainer}>
+                    <Text>1</Text>
+                </View>
+                <Text style={{fontWeight:'600'}}>{basketDish.name}</Text>
+                <Text style={{marginLeft: "auto"}}>${basketDish.price}</Text>
+                
+        </View>
+    )
+}
+
 const Basket = () => {
-
-    const [quantity,setQuantity] = useState(1);
-
-    const onMinus = () => {
-        if (quantity >= 1) {
-            setQuantity(quantity - 1);
-        }
-        
-        
-    }
-    const onPlus = () => {
-        setQuantity(quantity + 1);
-    }
-    const getTotal = () => {
-        return (dish.price * quantity).toFixed(2);
-    }
 
     return (
         <View style={styles.page}>
             <FontAwesome5 name="arrow-left" size={24} color="black" />
             <Text style={styles.name}>{restaurant.name}</Text>
-            
+            <Text style={{fontSize:19, fontWeight: "bold", marginTop: 20}}>Your Items</Text>
+            <FlatList 
+                data={restaurant.dishes} 
+                renderItem={({item}) => <BasketDishItem basketDish={item}/>}
+
+            />
             <View style={styles.seperator}></View>
             
+           
 
             <View style={styles.button}>
                 <Text style={styles.buttonText}>Create order</Text>
@@ -65,7 +70,7 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         alignItems: 'center',
         justifyContent: 'center',
-        marginTop: 50,
+        marginVertical: 15,
     },
     quantity: {
         fontSize: 25,
@@ -83,6 +88,13 @@ const styles = StyleSheet.create({
         fontWeight: "600",
         fontSize: 18,
     },
+    quantityContainer: {
+        marginRight: 10,
+        backgroundColor: "lightgray",
+        paddingHorizontal: 5,
+        paddingVertical:2,
+        borderRadius: 3,
+    }
 });
 
 
